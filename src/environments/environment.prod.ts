fa3490d7fb7782866de2e139ba0f21c13e41db73
Vincent <joevincentg24@gmail.com>
2022-05-20 23:43:33 +0100

    deployment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBZNSOg1TSKDC3_xIAT5pywvHBtJzxbLTw",
+    authDomain: "daw2-angular-firebase.firebaseapp.com",
+    projectId: "daw2-angular-firebase",
+    storageBucket: "daw2-angular-firebase.appspot.com",
+    messagingSenderId: "158237726164",
+    appId: "1:158237726164:web:e4715e9aba555bcd8ef241"
+  }
 };
